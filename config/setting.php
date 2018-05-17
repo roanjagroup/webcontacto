eb7d03cdc86c949ce60bfec026221453ced7456b
Roanja Group <info@roanja.com>
2018-05-17 18:49:48 +0200

    Update setting.php

--- config/setting.php
+++ config/setting.php
@@ -10,7 +10,7 @@
 
 /*conexción Local*/
 define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'incidencias');
+define('_DB_NAME_', 'webcontacto');
 define('_DB_USER_', 'root');
 define('_DB_PASSWD_', '');
 define('_DB_CHARSET_','utf-8'); 

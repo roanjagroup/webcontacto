3a14e78413fe8377443b8b652bb0ea6e21756531
Roanja Group <info@roanja.com>
2018-05-17 18:49:24 +0200

    Update setting.php

--- config/setting.php
+++ config/setting.php
@@ -5,11 +5,7 @@
 *
 */
 /* conexción remoto
-define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'incidencias');
-define('_DB_USER_', 'root');
-define('_DB_PASSWD_', 'Itrps#15');
-define('_DB_CHARSET_','utf-8'); 
+
 */
 
 /*conexción Local*/
